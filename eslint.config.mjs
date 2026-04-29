51c4a5f82cb29ed7c8a1371dadc258999fdea0b3
electronica-frog <bautiarmanielectronica@gmail.com>
2026-04-29 19:04:21 +0000

    fix: revertir eslint.config.mjs a version funcional (5c9799a)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,15 +9,15 @@ const __dirname = dirname(__filename);
 const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
   rules: {
     // TypeScript rules
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/prefer-as-const": "off",
     "@typescript-eslint/no-unused-disable-directive": "off",
     
     // React rules
-    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/exhaustive-deps": "off",
     "react-hooks/purity": "off",
     "react/no-unescaped-entities": "off",
     "react/display-name": "off",
@@ -25,23 +25,23 @@ const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
     "react-compiler/react-compiler": "off",
     
     // Next.js rules
-    "@next/next/no-img-element": "warn",
+    "@next/next/no-img-element": "off",
     "@next/next/no-html-link-for-pages": "off",
     
     // General JavaScript rules
-    "prefer-const": "warn",
-    "no-unused-vars": "off", // handled by @typescript-eslint/no-unused-vars
+    "prefer-const": "off",
+    "no-unused-vars": "off",
     "no-console": "off",
-    "no-debugger": "warn",
-    "no-empty": "warn",
-    "no-irregular-whitespace": "warn",
+    "no-debugger": "off",
+    "no-empty": "off",
+    "no-irregular-whitespace": "off",
     "no-case-declarations": "off",
     "no-fallthrough": "off",
-    "no-mixed-spaces-and-tabs": "warn",
+    "no-mixed-spaces-and-tabs": "off",
     "no-redeclare": "off",
     "no-undef": "off",
-    "no-unreachable": "warn",
-    "no-useless-escape": "warn",
+    "no-unreachable": "off",
+    "no-useless-escape": "off",
   },
 }, {
   ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", "examples/**", "skills"]
